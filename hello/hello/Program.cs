94f42e5715286c188b790869559068fd2d2ee500
mateuszkalisz <kaliszmat1@gmail.com>
2018-10-15 18:23:31 +0200

    cd.. odc 4

--- hello/hello/Program.cs
+++ hello/hello/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace hello
 {
@@ -10,8 +6,17 @@ namespace hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
+
+            string name = args[0];
+            Console.WriteLine("Witaj, " + name);
+            
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Witaj " + item);
+                
+            //}            
         }
+
+
     }
 }
