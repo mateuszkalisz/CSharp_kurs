e2b6d5eb9acd807097bfe2d65c57c954f763245c
mateuszkalisz <kaliszmat1@gmail.com>
2018-10-16 14:37:58 +0200

    lesson 7

---
+++ Lekcja7/Lekcja7/StudentDiary.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja7
+{
+    class StudentDiary
+    {
+        //stan(zmienne - pola)
+        List<float> ratings; //utworzenie listy ocen zmiennoprzecinkowych typu generycznego
+
+        //zachowania
+
+        /// <summary>
+        /// dodaje ocene do listy ocen
+        /// </summary>
+        /// <param name="rating"></param>
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        /// <summary>
+        /// oblicza srednia z ocen w dzienniczku
+        /// </summary>
+        /// <returns></returns>
+        public float CalculateAverage()
+        {
+
+            float sum = 0;
+            float avg = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+            avg = sum / ratings.Count();
+
+            return avg;
+        }
+
+        /// <summary>
+        /// oblicza max liczbe z dzienniczka
+        /// </summary>
+        /// <returns></returns>
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        /// <summary>
+        /// oblicza min liczbe z dzienniczka
+        /// </summary>
+        /// <returns></returns>
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+
+    }
+}
