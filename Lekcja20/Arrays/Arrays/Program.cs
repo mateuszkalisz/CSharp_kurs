5d716257b685def15f043d1cbed00d1fce92a0bf
mateuszkalisz <kaliszmat1@gmail.com>
2018-10-19 14:02:54 +0200

    lesson 20 - arrays

---
+++ Lekcja20/Arrays/Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int numberStudents = 5; // utworzenie zmiennej i przypisanie do niej 5, żeby w późniejszym etapie utworzyc tablice 5 elementowa
+            int[] scores = new int[numberStudents]; // utworzenie zmiennej i przypisanie/utworzenie dla niej tablicy o liczbie elementów takiej jaka zadeklarowana zostala w numberstudents (czyli 5) 
+
+            scores[0] = 1;
+            scores[1] = 2;
+            scores[2] = 15;
+            scores[3] = 3;
+            scores[4] = 14;
+
+            scores[0] = int.Parse(Console.ReadLine());
+            scores[1] = int.Parse(Console.ReadLine());
+            scores[2] = int.Parse(Console.ReadLine());
+            scores[3] = int.Parse(Console.ReadLine());
+            scores[4] = int.Parse(Console.ReadLine());
+
+
+            int totalScore = 0;
+
+            foreach (var score in scores)
+            {
+                totalScore += score;
+            }
+
+            double averageScore = totalScore / scores.Length;
+
+            Console.WriteLine("Suma wszystkich elementow tablicy: " + totalScore);
+            Console.WriteLine("Srednia wszystkich elementow tablicy: " + averageScore);
+            Console.ReadKey();
+
+
+            //int[] blabla = new int[5]; //utworzenie zmiennej blabla i przypisanie/utworzenie dla niej tablicy 5 elementowej
+
+
+
+
+        }
+    }
+}
---
+++ Lekcja7/Lekcja10/Odcinek10/DiaryStatistics.cs
@@ -0,0 +1,9 @@
+﻿namespace Odcinek10
+{
+    public class DiaryStatistics
+    {
+       public float AverageGrade;
+       public float MaxGrade;
+       public float MinGrade;
+    }
+}
