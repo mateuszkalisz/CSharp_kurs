4c976da0c65e83a51cc8eec9138441e66a126f2a
mateuszkalisz <kaliszmat1@gmail.com>
2018-10-23 22:30:55 +0200

    throw & try and catch

---
+++ Lekcja31/wyjatekThrow/wyjatekThrow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wyjatekThrow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wiek: ");
+            age wiek = new age();
+            wiek.ages = int.Parse(Console.ReadLine());
+                                   
+            try
+            {
+                wiek.agesMethod();
+            }
+
+            catch(ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lekcja31/wyjatekThrow/wyjatekThrow/age.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wyjatekThrow
+{
+    class age
+    {
+        public int ages;
+        
+        public void agesMethod()
+        {
+
+            if (ages>=18)
+            {
+                Console.WriteLine("Jestes pelnoletni");
+            }
+
+            else if(ages>0)
+            {
+                Console.WriteLine("Jesteś niepełnoletni");
+            }
+
+            else
+            {
+                throw new ArgumentException("Podaj poprawny wiek!");
+            }
+                          
+        }
+
+    }
+}
