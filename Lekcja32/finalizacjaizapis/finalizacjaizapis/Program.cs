673fe3007666e156296d2aea504bc480b7531f53
mateuszkalisz <kaliszmat1@gmail.com>
2018-10-24 22:21:16 +0200

    lesson 32 - save file & finally function

---
+++ Lekcja32/finalizacjaizapis/finalizacjaizapis/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace finalizacjaizapis
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //StreamWriter file = new StreamWriter("ZapisPliku.txt");
+
+            
+                jakasklasacs wiek = new jakasklasacs();
+
+                Console.WriteLine("Podaj wiek: ");
+                wiek.age = int.Parse(Console.ReadLine());
+
+            using (StreamWriter file = new StreamWriter("pliktekstowy.txt", true))
+            {
+                wiek.Metoda();
+                string y = wiek.x;
+                file.WriteLine(y);
+                
+            }
+
+            wiek.Metoda2();
+            Console.ReadKey();
+
+
+                    //finally
+                    //{
+                    //    file.Close();
+                    //}
+                
+            
+        }
+    }
+}
+
+                   
+            
+                
+            
+            
+
+
+        
+
+
+    
+
---
+++ Lekcja32/finalizacjaizapis/finalizacjaizapis/jakasklasacs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace finalizacjaizapis
+{
+    class jakasklasacs
+    {
+        public int age;
+        public string x;
+        
+        
+        public void Metoda()
+        {
+            if (age >= 18)
+            {
+                
+                string y = "Jesteś pełnoletni";
+                Console.WriteLine(y);
+                x = y;
+                
+             }
+            else if (age > 0)
+            {
+                string y = "Jesteś niepełnoletni";
+                Console.WriteLine(y);
+                x = y;
+            }
+
+            else
+            {
+                string y = "Wpisałeś zły wiek";
+                Console.WriteLine(y);
+                x = y;
+            
+            }
+                        
+        }
+
+        public void Metoda2()
+        {
+            if(age <0) throw new AggregateException(x);
+        }
+
+    }
+}
