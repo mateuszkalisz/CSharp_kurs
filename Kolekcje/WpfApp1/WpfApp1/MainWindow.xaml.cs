6b7b34a2916c8c95dc5d38caa6e6c1f5f876ff1d
mateuszkalisz <kaliszmat1@gmail.com>
2018-11-01 02:09:29 +0100

    enumy i kolekcje

---
+++ Kolekcje/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Kolekcje();
+            WypisanieKolekcji();
+        }
+
+        #region Kolekcje
+
+        int[] tablica = new int[10];
+        List<int> lista = new List<int>();
+
+        void Kolekcje()
+        {
+            for (int i = 0; i < tablica.Length; i=i+2)
+            {
+                tablica[i] = 1;
+            }
+
+            for (int i = 1; i < tablica.Length; i = i + 2)
+            {
+                tablica[i] = 2;
+            }
+
+            for (int i = 5; i < 10; i++)
+            {
+                lista.Add(i);
+            }
+
+        }
+
+        void WypisanieKolekcji()
+        {
+            ArrayListView.ItemsSource = tablica;
+            ListListView.ItemsSource = lista;
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ enumy/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            EnumGetValue();
+        }
+
+        #region enumy
+
+        enum CharacterConditions 
+        {
+            Run,
+            Jump,
+            Swim
+        }
+
+        void EnumGetValue()
+        {
+            CharacterConditions enumix = CharacterConditions.Run;
+
+            switch (enumix)
+            {
+                case CharacterConditions.Run:
+                    break;
+                case CharacterConditions.Jump:
+                    break;
+                case CharacterConditions.Swim:
+                    break;
+                default:
+                    break;
+            }
+
+            var enumvalues = Enum.GetValues(typeof(CharacterConditions));
+            enumListView.ItemsSource = enumvalues;
+            enumTextBlock.Text = enumix.ToString();
+        }
+
+        #endregion
+
+    }
+}
