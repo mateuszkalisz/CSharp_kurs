0d8373238ddf580478dcf7b91a7dd402c3b74ce2
mateuszkalisz <kaliszmat1@gmail.com>
2018-11-06 21:21:58 +0100

    dictionary

--- AntLifeConsole/AntLifeConsole/AntLifeConsole/Program.cs
+++ AntLifeConsole/AntLifeConsole/AntLifeConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AntLifePCL.TestClass;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,70 +12,104 @@ namespace AntLifeConsole
         static void Main(string[] args)
         {
             #region Tablica jednowymiarowa
-            int[] a = new int[3];
+            //int[] a = new int[3];
 
-            a[0] = 1;
-            a[1] = 3;
-            a[2] = 5;
+            //a[0] = 1;
+            //a[1] = 3;
+            //a[2] = 5;
 
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.WriteLine(a[i]);
-            }
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.WriteLine(a[i]);
+            //}
             #endregion
 
             #region Tablica dwuwymiarowa
-            int[,] b = new int[10, 10];
+            //int[,] b = new int[10, 10];
 
-            b[0, 0] = 10;
-            b[0, 2] = 20;
-            b[1, 3] = 30;
-            b[2, 4] = 40;
-            b[5, 6] = 50;
+            //b[0, 0] = 10;
+            //b[0, 2] = 20;
+            //b[1, 3] = 30;
+            //b[2, 4] = 40;
+            //b[5, 6] = 50;
 
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    Console.WriteLine(b[i,j]);
-                }
-            }
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        Console.WriteLine(b[i,j]);
+            //    }
+            //}
             #endregion
 
             #region Tablica wyszczerbiona
-            int[][] c = new int[5][];
-            c[0] = new int[3];
-            c[1] = new int[5];
-            c[2] = new int[2];
-            c[3] = new int[4];
-            c[4] = new int[2];
+            //int[][] c = new int[5][];
+            //c[0] = new int[3];
+            //c[1] = new int[5];
+            //c[2] = new int[2];
+            //c[3] = new int[4];
+            //c[4] = new int[2];
 
-            c[0][0] = 1;
-            c[0][1] = 1;
-            c[0][2] = 1;
-            c[1][0] = 2;
-            c[1][1] = 2;
-            c[1][2] = 2;
-            c[1][3] = 2;
-            c[1][4] = 2;
-            c[2][0] = 3;
-            c[2][1] = 3;
-            c[3][0] = 4;
-            c[3][1] = 4;
-            c[3][2] = 4;
-            c[3][3] = 4;
-            c[4][0] = 5;
-            c[4][1] = 5;
+            //c[0][0] = 1;
+            //c[0][1] = 1;
+            //c[0][2] = 1;
+            //c[1][0] = 2;
+            //c[1][1] = 2;
+            //c[1][2] = 2;
+            //c[1][3] = 2;
+            //c[1][4] = 2;
+            //c[2][0] = 3;
+            //c[2][1] = 3;
+            //c[3][0] = 4;
+            //c[3][1] = 4;
+            //c[3][2] = 4;
+            //c[3][3] = 4;
+            //c[4][0] = 5;
+            //c[4][1] = 5;
 
-            for (int i = 0; i < c.Length; i++)
+            //for (int i = 0; i < c.Length; i++)
+            //{
+            //    for (int j = 0; j < c[i].Length; j++)
+            //    {
+            //        Console.WriteLine(c[i][j]);
+            //    }
+            //}
+
+            #endregion
+
+            #region Guid
+
+            //int x = default(int); // defaultowo 0;
+
+            //Console.WriteLine("x = " + x);
+
+            //Guid y = new Guid();
+
+            //y = default(Guid); // defaultowo 000-000-000000.... itd
+
+            //Console.WriteLine("y = " + y);
+
+            #endregion
+
+            #region Dictionary
+
+            Dictionary<int, TestClass> a = new Dictionary<int, TestClass>();
+
+            a.Add(1, new TestClass { TestClassID = 1, TestClassName = "test1" });
+            a.Add(2, new TestClass { TestClassID = 11, TestClassName = "test2" });
+            a.Add(3, new TestClass { TestClassID = 111, TestClassName = "test3" });
+            a.Add(4, new TestClass { TestClassID = 1111, TestClassName = "test4" });
+
+            foreach (var i in a)
             {
-                for (int j = 0; j < c[i].Length; j++)
-                {
-                    Console.WriteLine(c[i][j]);
-                }
+                Console.WriteLine(i.Key + " | " + i.Value.TestClassID + " | " + i.Value.TestClassName);
             }
 
-             #endregion
+            //Console.WriteLine(a[1].TestClassName); //pierwszyy element wg klucza (indeksu)
+            //Console.WriteLine(a.ElementAt(0).Value.TestClassName; // pierwszy element wg indeksu jak dla tablic
+
+            #endregion
+
 
             Console.ReadKey();
         }
