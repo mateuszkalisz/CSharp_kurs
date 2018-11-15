0d12e84bc58f193b76cd76561425fb8e544569e8
mateuszkalisz <kaliszmat1@gmail.com>
2018-11-15 21:23:45 +0100

    delegate - lessons

---
+++ ConsoleAppDelegate/ConsoleAppDelegate/ConsoleAppDelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDelegate
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            SomeLongRunningData SomeData = new SomeLongRunningData();
+            SomeData.SomeMethod(CallBackMethod);
+
+        }
+
+        static void CallBackMethod(int i)
+        {
+            Console.WriteLine(i + "bla");
+        }
+
+    
+
+    }
+
+    class SomeLongRunningData
+    {
+        public delegate void CallBack(int i);
+
+        public void SomeMethod(CallBack obj)
+        {
+            Console.WriteLine("Robi się coś tam i inne rzeczy w tle... ");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                obj(i);
+                
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
