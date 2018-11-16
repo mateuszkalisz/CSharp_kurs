75b99736b06aebb378fb7e73ac543b00c2332971
mateuszkalisz <kaliszmat1@gmail.com>
2018-11-16 20:21:23 +0100

    func<> action<> predicate<> - delegate lessons

---
+++ ConsoleAppDelegate/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toys newToy = new Toys();
+            newToy.AddToys(CallBackToysMethod);
+
+        }
+
+        static void CallBackToysMethod(int i)
+        {
+            Console.WriteLine("nowa zabawka nr: " + (i + 1));
+        }
+
+    }
+
+    public class Toys
+    {
+        public delegate void CallBackToysDelegate(int i);
+
+        Dictionary<int, string> KindOfToys = new Dictionary<int, string>();
+                        
+        public void AddToys(CallBackToysDelegate obj)
+        {
+            Console.WriteLine("Witamy nową zabawkę!");
+
+            for (int i = 1; i < 10; i++)
+            {
+                KindOfToys.Add(i, "nowa zabawka" + (i+1));
+                obj(i);
+            }
+
+            Console.WriteLine("Dictionary nr 1: " + KindOfToys[1]);
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
---
+++ DelegatyCD/ConsoleAppDelegate/ConsoleAppDelegate/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDelegate
+{
+    class Program
+    {
+        //delegate double CalculateAreaPointer(double r); //tworzymy delegata - musi byc utworzony do wszystkiego tutaj poza func, action, predicate
+        //static CalculateAreaPointer cpointer = CalculateArea; //tworzymy zmienna - wskaznik delegata, ktory wskazuje na metode,
+        
+        static void Main(string[] args)
+        {
+            //double area = cpointer.Invoke(20);
+
+            //tutaj wykorzystanie metody anonimowej
+            //CalculateAreaPointer cpointer = new CalculateAreaPointer(delegate (double r)
+            //    {
+            //        return Math.PI * r * r;
+            //    });
+
+            //double area = cpointer(20);
+
+            //wykorzystanie lambdy - zastąpienie zapisu
+            //CalculateAreaPointer Cpointer = r => Math.PI * r * r;
+            //double area = Cpointer(20);
+
+            //func skraca tworzenie delegatow + jeszcze krocej dzieki lambdy czyli => , to samo jak za pomoca delegatow - to jest delegat ale duzo szybszy zapis
+            //Func<double, double> Cpointer = r => Math.PI * r * r;
+            //double area = Cpointer(20);
+
+            //Action - delegat typu void
+            //Action<string> MyAction = s => Console.WriteLine(s);
+            //MyAction("to ja");
+
+            //Predicate - rzadko uzywany, ale tez delegat - taki nietypowy delegat ktory zwraca tylko true or false
+            Predicate<string> CheckIfStringIsGreatherThan5 = x => x.Length > 5;
+            Console.WriteLine(CheckIfStringIsGreatherThan5("abdulhuehue"));
+            Console.ReadKey();
+
+        }
+
+        //static double CalculateArea(double r) //jakas metoda
+        //{
+        //    return Math.PI * r * r;
+        //}
+    }
+}
