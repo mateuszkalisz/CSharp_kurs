94982a6e98da17899f2ace99452da10955c9c8fe
mateuszkalisz <kaliszmat1@gmail.com>
2018-11-18 20:53:17 +0100

    ref vs out and constant vs readonly - lessons

---
+++ constantVSreadonly/ConsoleAppConstAndReadonly/ConsoleAppConstAndReadonly/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppConstAndReadonly
+{
+
+    /// <summary>
+    /// Constant vs Readonly
+    /// </summary>
+
+    class Program
+    {
+        public const int thisIsConst = 10; // const - jest tworzone w trakcie kompilacji, tego w ogole nie mozemy zmienic nawet w konstruktorze
+        public static readonly double thisIsReadonly = 11; // readonly - jest przypisywane w runtime'ie, mozna zmienic w konstruktorze - czyli w trakcie tworzenia danej klasy
+
+        public const string MyKey = "MyKey";
+        public static readonly double betterPI = Math.PI;
+
+        static Program()
+        {
+            thisIsReadonly = 10.11;
+            betterPI = Convert.ToDouble(ConfigurationManager.AppSettings[MyKey], CultureInfo.InvariantCulture);
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ refvsout/ConsoleApprefvsout/ConsoleApprefvsout/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApprefvsout
+{
+    class Program
+    {
+        /// <summary>
+        /// 1. out i ref sa slowami kluczowymi w c# ktore pomagaja przekazac referencje
+        /// 2. ref przekazuje wartosc i referencje (dziala dwustronnie)
+        /// 3. out przekazuje referencje ale nie wartosci (dziala jednostronnie)
+        /// </summary>
+        /// 
+        static int outsideInt = 20;
+
+        static void Main(string[] args)
+        {
+            int outsideInt = 20;
+            SomeMethod(out outsideInt);
+
+            Console.WriteLine(outsideInt);
+            Console.ReadKey();
+        }
+
+        private static void SomeMethod(out int insideInt)
+        {
+            insideInt = 0;
+            insideInt = insideInt + 10;
+        }
+    }
+}
+
+
