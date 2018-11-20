1bf4fc233a57771dfa2f38d9385fae2661034b95
mateuszkalisz <kaliszmat1@gmail.com>
2018-11-20 18:36:16 +0100

    equals vs == and ienumberable vs ienumberator

---
+++ Equals/ConsoleAppEquals/ConsoleAppEquals/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object a = "jaka jest roznica pomiedzy equals i ==";
+            object a1 = new string("jaka jest roznica pomiedzy equals i ==".ToCharArray());
+
+            Console.WriteLine(a == a1);  // porownuje referencje obiektu
+            Console.WriteLine(a.Equals(a1)); // porownuje wartosci
+
+            Console.WriteLine("-----------------------");
+
+            string b = "blabla";
+            string c = "blabla";
+
+            Console.WriteLine(b==c); // tylko w stringu porownuje wartosci, w pozostalych referencje
+            Console.WriteLine(b.Equals(c)); // zawsze porownuje wartosci
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ IEnumer/ConsoleAppIEnumberableVSIEnumerator/ConsoleAppIEnumberableVSIEnumerator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppIEnumberableVSIEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            List<int> myList = new List<int>();
+
+
+            myList.Add(100);
+            myList.Add(200);
+            myList.Add(300);
+            myList.Add(400);
+            myList.Add(500);
+            myList.Add(600);
+
+            IEnumerable<int> ienumerable = myList.AsEnumerable(); // ienumerable nie wie w jakim stanie sie znajduje, dziedziczy z ienumerator wiec robi praktycznie to samo
+
+            //foreach (var item in ienumerable)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            IEnumerator<int> ienumerator = myList.GetEnumerator(); // ienumerator - wie w jakim stanie sie znajduje
+
+            //while (ienumerator.MoveNext())
+            //{
+            //    Console.WriteLine(ienumerator.Current.ToString());
+            //}
+
+            LoopTo300(ienumerator);
+            LoopTo400(ienumerable);
+
+            Console.ReadKey();
+
+        }
+
+        static void LoopTo300(IEnumerator<int> e)
+        {
+            while(e.MoveNext())
+            {
+                Console.WriteLine(e.Current.ToString());
+                if(e.Current >299)
+                {
+                    LoopAbove300(e);
+                }
+            }
+        }
+
+        static void LoopAbove300(IEnumerator<int> e)
+        {
+            while(e.MoveNext())
+            {
+                Console.WriteLine(e.Current.ToString());
+            }
+        }
+
+        static void LoopTo400(IEnumerable<int> eble)
+        {
+            foreach (var item in eble)
+            {
+                Console.WriteLine(item.ToString());
+                if (item > 399)
+                    LoopAbove400(eble);
+
+            }
+        }
+
+        static void LoopAbove400(IEnumerable<int> eble)
+        {
+            foreach (var item in eble)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+
+        
+    }
+}
