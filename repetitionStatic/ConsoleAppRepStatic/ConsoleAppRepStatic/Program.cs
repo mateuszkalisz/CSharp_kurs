427df08b1f7b16457db47a2659434c49fd92a28f
mateuszkalisz <kaliszmat1@gmail.com>
2018-12-05 23:26:24 +0100

    repetition of static class (fields and methods)

---
+++ repetitionStatic/ConsoleAppRepStatic/ConsoleAppRepStatic/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRepStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StaticMethods.PI);
+            Console.WriteLine(StaticMethods.EulerValue);
+            Console.WriteLine(StaticMethods.GoldValue);
+
+            Console.WriteLine("Podaj promień koła: ");
+
+            int r = int.Parse(Console.ReadLine());
+
+            double result = StaticMethods.CircleArea(r);
+
+            Console.WriteLine("Pole kola wynosi: " + result);
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+    static class StaticMethods
+    {
+        public static double PI = 3.14;
+        public static double EulerValue = 0.577;
+        public static double GoldValue = 1.61803;
+
+        public static double CircleArea(int R)
+        {
+            return PI * R * R;
+        }
+    }
+
+
+}
