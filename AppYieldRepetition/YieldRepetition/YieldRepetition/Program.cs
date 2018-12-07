30987e87f3d15e753efe6096ccb3041545c6536d
mateuszkalisz <kaliszmat1@gmail.com>
2018-12-07 22:13:26 +0100

    yield

---
+++ AppYieldRepetition/YieldRepetition/YieldRepetition/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldRepetition
+{
+    class Program
+    {
+        static List<int> myList = new List<int>();
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("custom iteration");
+
+            FillList();
+
+            foreach (var item in Filter())
+            { 
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("statefull iteration");
+
+            foreach (var item in Total())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+        }
+
+        static void FillList()
+        {
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+            myList.Add(6);
+            myList.Add(7);
+        }
+
+
+        // Custom Iteration
+        static IEnumerable<int> Filter()
+        {
+            foreach (var item in myList)
+            {
+                if (item > 3)
+                    yield return item;
+            }
+        }
+
+        // statefull iteration
+
+        static IEnumerable<int> Total()
+        {
+            int totalSUM = 0;
+
+            foreach (var item in myList)
+            {
+                totalSUM += item;
+                yield return totalSUM;
+            }
+
+        }
+
+    }
+}
---
+++ AppYieldRepetition/YieldRepetition/YieldRepetition/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("YieldRepetition")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YieldRepetition")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("17a666e0-d60f-4861-9c92-48e2b980fd6a")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
