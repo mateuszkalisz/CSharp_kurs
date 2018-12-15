bfb4e906f8e12f373b29f63989078418af7a8859
MK <kaliszmat1@gmail.com>
2018-12-16 00:37:04 +0100

    ref i out - repetition

---
+++ repetitionOutRef/repOutRef/repOutRef/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace repOutRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int outsideValue = 20;
+            SomeMethod(out outsideValue);
+
+            Console.WriteLine(outsideValue);
+            Console.ReadKey();
+
+        }
+
+        private static void SomeMethod(out int insideValue)
+        {
+            insideValue = 0;
+            insideValue = insideValue + 10;
+        }
+    }
+}
+
+// ref - przekazuje i wartosc i referencje
+// out - przekazuje tylko referencje
+// brak - przekazuje tylko wartosc
