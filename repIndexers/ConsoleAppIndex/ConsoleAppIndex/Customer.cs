d1327dc723608e2a0cd0f7512b32285a20cb26a0
MK <kaliszmat1@gmail.com>
2018-12-16 18:15:19 +0100

    repetition of indexers

---
+++ repIndexers/ConsoleAppIndex/ConsoleAppIndex/Customer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppIndex
+{
+    class Customer
+    {
+        private List<Address> CustomerAddresses = new List<Address>();
+
+        public Customer()
+        {
+            CustomerAddresses.Add(new Address { Pin = 123456, Street = "JP", City = "Elk" });
+            CustomerAddresses.Add(new Address { Pin = 654321, Street = "Pelczynskiego", City = "Warsaw" });
+            CustomerAddresses.Add(new Address { Pin = 123456, Street = "Okulickiego", City = "Tomaszów Lubelski" });
+        }
+
+        public Address GetAddresses(int Pin)
+        {
+            foreach (Address item in CustomerAddresses)
+            {
+                if (item.Pin == Pin)
+                    return item;
+            }
+            return null;
+        }
+
+        public Address this[int Pin]
+        {
+            get
+            {
+                foreach (Address item in CustomerAddresses)
+                {
+                    if (item.Pin == Pin)
+                        return item;
+                }
+                return null;
+            }
+        }
+    }
+
+    
+
+
+    public class Address
+    {
+        public int Pin { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ repIndexers/ConsoleAppIndex/ConsoleAppIndex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleAppIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Customer blabla = new Customer();
+
+            var a = blabla[123456];
+        }
+    }
+}
