71f717e73dea01b85a826bd5b24d625b19b0b9a5
MK <kaliszmat1@gmail.com>
2018-12-20 22:49:40 +0100

    polimorphism lesson 2

---
+++ rep_poli/ConsoleApp1Polimorfizm/ConsoleApp1Polimorfizm/Base.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1Polimorfizm
+{
+    public class Base
+    {
+        //fields
+        public virtual string Name { get; set; }
+        public virtual float Longitude { get; set; }
+        public virtual float Latitude { get; set; }
+
+        //method
+
+        public virtual void CreateBase(string Name, float Longitude, float Latitude)
+        {
+            Console.WriteLine("Basebody+ Set name: " + Name);
+            Console.WriteLine("Basebody+ Set Localization: " + Longitude + " : " + Latitude);
+        }
+    }
+}
