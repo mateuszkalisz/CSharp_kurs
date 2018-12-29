f0b779199ac3d06319996741d1d9e5334aed68b8
MK <kaliszmat1@gmail.com>
2018-12-29 19:45:24 +0100

    delegate by callback - Repetition

---
+++ rep_delegate/RepetitionDelegate/RepetitionDelegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RepetitionDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OtherBasicMehod sm = new OtherBasicMehod();
+            sm.SomeMethod(CallBackMethod);
+            Console.ReadKey();
+        }
+
+        static void CallBackMethod(int i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    class OtherBasicMehod
+    {
+        public delegate void CallBack(int i);
+
+        public void SomeMethod(CallBack obj)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                //robi cos sie w tle....
+                obj(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
