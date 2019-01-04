3c0c456d6cdb08bcad3f93c3f742268173f3c87a
MK <kaliszmat1@gmail.com>
2019-01-04 22:58:18 +0100

    queue collection

---
+++ queueANDstack/queue_stack/queue_stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace queue_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queueCollection = new Queue<int>();
+            queueCollection.Enqueue(1);
+            queueCollection.Enqueue(2);
+            queueCollection.Enqueue(3);
+
+            Console.WriteLine(queueCollection.Peek());
+            Console.WriteLine(queueCollection.Dequeue());
+            Console.WriteLine(queueCollection.Dequeue());
+
+
+            Console.ReadKey();        }
+    }
+}
---
+++ rep_delegate/Func_Action_Predicate_repetition/Func_Action_Predicate_repetition/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Func_Action_Predicate_repetition
+{
+    class Program
+    {
+        //delegate double CalculateAreaDel(double r);
+        //static CalculateAreaDel cpointer = CalculateArea;
+
+        static void Main(string[] args)
+        {
+            //// double area = cpointer.Invoke(20);
+            //CalculateAreaDel cpointer = new CalculateAreaDel(
+            //                            delegate (double r)
+            //                            {
+            //                                return Math.PI * r * r;
+            //                            }
+            //    );
+
+            //double area = cpointer(20);
+
+            //CalculateAreaDel cpointer = r => Math.PI * r * r;
+            //double area = cpointer(20);
+
+            Func<double, double> cpointer = r => Math.PI * r * r;
+            double area = cpointer(20);
+
+            Action<string> MyAction = s => Console.WriteLine(s);
+            MyAction("to ja");
+
+            Predicate<string> CheckIfStringIsGreatherThanS = g => g.Length > 5;
+            Console.WriteLine(CheckIfStringIsGreatherThanS("ala ma kota"));
+        }
+
+        //static double CalculateArea(double r)
+        //{
+        //    return Math.PI * r * r;
+        //}
+    }
+}
