829d9b3fe446c753b851b0f69d13b890a2d01447
MK <kaliszmat1@gmail.com>
2019-01-05 13:36:07 +0100

    stack collection in c#

--- queueANDstack/queue_stack/queue_stack/Program.cs
+++ queueANDstack/queue_stack/queue_stack/Program.cs
@@ -7,16 +7,35 @@ namespace queue_stack
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kolejka");
+            //odczytywanie elementów z kolejki jest mozliwe od pierwszego wrzuconego elementu (dostępny jest wierzchni element)
+
             Queue<int> queueCollection = new Queue<int>();
             queueCollection.Enqueue(1);
             queueCollection.Enqueue(2);
             queueCollection.Enqueue(3);
 
             Console.WriteLine(queueCollection.Peek());
+
+            Console.WriteLine(queueCollection.Dequeue());
             Console.WriteLine(queueCollection.Dequeue());
             Console.WriteLine(queueCollection.Dequeue());
 
+            Console.WriteLine("stos");
+            //działa odwrotnie, do kolejki, to co bylo wrzucone to od ostatniego elementu mozna zaczac odczytywac (dostępny jest spodni element)
 
-            Console.ReadKey();        }
+            Stack<int> stackCollection = new Stack<int>();
+            stackCollection.Push(1);
+            stackCollection.Push(2);
+            stackCollection.Push(3);
+
+            Console.WriteLine(stackCollection.Peek());
+
+            Console.WriteLine(stackCollection.Pop());
+            Console.WriteLine(stackCollection.Pop());
+            Console.WriteLine(stackCollection.Pop());
+
+            Console.ReadKey();
+        }
     }
 }
